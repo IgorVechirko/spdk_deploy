2782080dee5131b3a3f51cb534265bef1d4fb5de
igor.vechirko <igor.vechirko@starwind.com>
2022-09-28 10:43:22 +0000

    add name space options

--- scripts/connect_remote_nodes.sh
+++ scripts/connect_remote_nodes.sh
@@ -11,8 +11,8 @@ self_path=$(pwd)
 
 cd $(get_var "spdk_path")
 
-sudo ./scripts/rpc.py bdev_ha_add_remote_node $(get_var "ha_name") --remote_node_id 2 --remote_node_type SYNC_REPLICA --remote_node_nqn $(get_var "nqn") --remote_node_channel "tcp 192.168.0.103 4421"
+sudo ./scripts/rpc.py bdev_ha_add_remote_node $(get_var "ha_name") --remote_node_id 2 --remote_node_type SYNC_REPLICA --remote_node_nqn $(get_var "nqn") --remote_node_nsid 1 --remote_node_channel "tcp 192.168.0.103 4421"
 
-#sudo ./scripts/rpc.py bdev_ha_add_remote_node $(get_var "ha_name") --remote_node_id 3 --remote_node_type SYNC_REPLICA --remote_node_nqn $(get_var "nqn") --remote_node_channel "tcp 192.168.0.131 4421"
+#sudo ./scripts/rpc.py bdev_ha_add_remote_node $(get_var "ha_name") --remote_node_id 3 --remote_node_type SYNC_REPLICA --remote_node_nqn $(get_var "nqn") --remote_node_nsid 1 --remote_node_channel "tcp 192.168.0.131 4421"
 
 cd $self_path
--- scripts/create_ha.sh
+++ scripts/create_ha.sh
@@ -11,7 +11,7 @@ self_path=$(pwd)
 
 cd $(get_var "spdk_path")
 
-sudo ./scripts/rpc.py bdev_ha_create_header $(get_var "ha_header") --device_id $(get_var "ha_dev_id") --device_size $(get_var "ha_size") --local_node_id $(get_var "local_node_id") --local_node_type SYNC_REPLICA --local_node_nqn $(get_var "nqn") --local_node_data_replica $(get_var "local_node_bdev")
+sudo ./scripts/rpc.py bdev_ha_create_header $(get_var "ha_header") --device_id $(get_var "ha_dev_id") --device_size $(get_var "ha_size") --local_node_id $(get_var "local_node_id") --local_node_type SYNC_REPLICA --local_node_nqn $(get_var "nqn") --local_node_nsid $(get_var "local_node_nsid") --local_node_data_replica $(get_var "local_node_bdev")
 sudo ./scripts/rpc.py bdev_ha_create $(get_var "ha_name") $(get_var "ha_header")
 
 sudo ./scripts/rpc.py nvmf_subsystem_add_ns $(get_var "nqn") $(get_var "ha_name")
