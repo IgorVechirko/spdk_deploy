e8caaa03c2101c70fde63d8308c70b682229a4bb
igor.vechirko <igor.vechirko@starwind.com>
2022-10-12 10:39:15 +0000

    update script

--- confluence_samples/ha_with_smb/node_2.sh
+++ confluence_samples/ha_with_smb/node_2.sh
@@ -7,10 +7,10 @@ sudo ./scripts/rpc.py nvmf_create_transport -t TCP
 
 sudo ./scripts/rpc.py bdev_malloc_create -b ram_bdev 1024 512
 
+sudo ./scripts/rpc.py nvmf_create_subsystem nqn.2016-06.sw.ha:node -a -s SPDK00000000000001 -d SPDK_Controller1 --ana-reporting
+
 sudo ./scripts/rpc.py bdev_ha_create_header /root/wc/ha/ha_header.json --device_id 1 --device_size 1GiB --local_node_id 2 --local_node_type SYNC_REPLICA --local_node_nqn nqn.2016-06.sw.ha:node --ha_creation_action create_new --local_node_data_replica ram_bdev
 sudo ./scripts/rpc.py bdev_ha_create Ha0 /root/wc/ha/ha_header.json
-
-sudo ./scripts/rpc.py nvmf_create_subsystem nqn.2016-06.sw.ha:node -a -s SPDK00000000000001 -d SPDK_Controller1 --ana-reporting
 
 #sudo ./scripts/rpc.py nvmf_subsystem_add_listener nqn.2016-06.sw.ha:node -t rdma-a 192.168.0.10 -s 4421
 sudo ./scripts/rpc.py nvmf_subsystem_add_listener nqn.2016-06.sw.ha:node -t tcp -a 192.168.0.103 -s 4421
@@ -18,7 +18,7 @@ sudo ./scripts/rpc.py nvmf_subsystem_add_listener nqn.2016-06.sw.ha:node -t tcp 
 
 sudo ./scripts/rpc.py bdev_ha_add_remote_node Ha0 --remote_node_id 1 --remote_node_type SYNC_REPLICA --remote_node_nqn nqn.2016-06.sw.ha:node --remote_node_channel "TCP 192.168.0.102 4421" #--remote_node_channel "RDMA 192.168.0.11 4421"
 
-sudo ./scripts/rpc.py bdev_ha_add_raft_smb_witness Ha0 --witness_domain "" --witness_username "igor" --witness_password "1234" --witness_server "192.168.0.131" --witness_share "witness_share_root" --witness_path ""
+sudo ./scripts/rpc.py bdev_ha_add_raft_smb_witness Ha0 --witness_domain "" --witness_username "root" --witness_password "1234" --witness_server "192.168.0.131" --witness_share "witness_share_root" --witness_path ""
 
 sudo ./scripts/rpc.py bdev_ha_set_raft_leader_election Ha0
 
