a8219eded456e38d162ea6a17b77b6448249a478
igor.vechirko <igor.vechirko@starwind.com>
2022-10-24 10:51:40 +0000

    add uuid to create scripts

--- scripts/create_ha.sh
+++ scripts/create_ha.sh
@@ -17,7 +17,17 @@ then
 	node_bdev=$(get_node $1 "nvme_bdev.name")
 fi
 
-sudo ./scripts/rpc.py bdev_ha_create_header $(get_node $1 "header_path") --device_id $(get_var "ha.id") --device_size $(get_var "ha.size") --local_node_id $(get_node $1 "id") --local_node_type SYNC_REPLICA --local_node_nqn $(get_var "ha.nqn") --local_node_nsid $(get_node $1 "ns_id") --local_node_data_replica $node_bdev --ha_creation_action create_new
+sudo ./scripts/rpc.py bdev_ha_create_header $(get_node $1 "header_path") \
+ --device_id $(get_var "ha.id")\
+ --device_size $(get_var "ha.size")\
+ --uuid $(get_var "ha.uuid")\
+ --local_node_id $(get_node $1 "id")\
+ --local_node_type SYNC_REPLICA\
+ --local_node_nqn $(get_var "ha.nqn")\
+ --local_node_nsid $(get_node $1 "ns_id")\
+ --local_node_data_replica $node_bdev\
+ --ha_creation_action create_new
+
 sudo ./scripts/rpc.py bdev_ha_create $(get_var "ha.name") $(get_node $1 "header_path")
 
 cd $self_path
