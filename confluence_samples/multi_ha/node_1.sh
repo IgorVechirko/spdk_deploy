836912a70191d9fc3f8d2b106b97ef00bd61c657
igor.vechirko <igor.vechirko@starwind.com>
2022-10-25 11:53:10 +0000

    update

--- confluence_samples/multi_ha/node_1.sh
+++ confluence_samples/multi_ha/node_1.sh
@@ -13,11 +13,11 @@ sudo ./scripts/rpc.py nvmf_create_subsystem nqn.2016-06.sw.ha1:node -a -s SPDK00
 sudo ./scripts/rpc.py nvmf_subsystem_add_listener nqn.2016-06.sw.ha1:node -t tcp -a 192.168.0.102 -s 4421
 sudo ./scripts/rpc.py nvmf_subsystem_add_listener nqn.2016-06.sw.ha1:node -t tcp -a 192.168.0.102 -s 4430
 
-sudo ./scripts/rpc.py bdev_ha_create_header ../ha/ha1_header.json --device_id 1 --local_node_id 1 --local_node_type SYNC_REPLICA --local_node_nqn nqn.2016-06.sw.ha1:node --local_node_data_replica ha1_1_node --ha_creation_action create_new
+sudo ./scripts/rpc.py bdev_ha_create_header ../ha/ha1_header.json --device_id 1 --device_nqn nqn.2016-06.sw.ha1:node --uuid 6d28acdf-7d3e-49b2-ac07-f5678160d7c3 --local_node_id 1 --local_node_type SYNC_REPLICA --local_node_data_replica ha1_1_node --ha_creation_action create_new
 
 sudo ./scripts/rpc.py bdev_ha_create Ha1 ../ha/ha1_header.json -c 0
 
-sudo ./scripts/rpc.py bdev_ha_add_remote_node Ha1 --remote_node_id 2 --remote_node_type SYNC_REPLICA --remote_node_nqn nqn.2016-06.sw.ha1:node --remote_node_channel "tcp 192.168.0.103 4421"
+sudo ./scripts/rpc.py bdev_ha_add_remote_node Ha1 --remote_node_id 2 --remote_node_type SYNC_REPLICA --remote_node_channel "tcp 192.168.0.103 4421"
 
 sudo ./scripts/rpc.py bdev_ha_set_raft_leader_election Ha1
 
@@ -31,10 +31,10 @@ sudo ./scripts/rpc.py nvmf_create_subsystem nqn.2016-06.sw.ha2:node -a -s SPDK00
 sudo ./scripts/rpc.py nvmf_subsystem_add_listener nqn.2016-06.sw.ha2:node -t tcp -a 192.168.0.102 -s 4421
 sudo ./scripts/rpc.py nvmf_subsystem_add_listener nqn.2016-06.sw.ha2:node -t tcp -a 192.168.0.102 -s 4430
 
-sudo ./scripts/rpc.py bdev_ha_create_header ../ha/ha2_header.json --device_id 2 --local_node_id 1 --local_node_type SYNC_REPLICA --local_node_nqn nqn.2016-06.sw.ha2:node --local_node_data_replica ha2_1_node --ha_creation_action create_new
+sudo ./scripts/rpc.py bdev_ha_create_header ../ha/ha2_header.json --device_id 2 --device_nqn nqn.2016-06.sw.ha2:node --uuid 6d28acdf-0000-49b2-ac07-f5678160d7c3 --local_node_id 1 --local_node_type SYNC_REPLICA --local_node_data_replica ha2_1_node --ha_creation_action create_new
 
 sudo ./scripts/rpc.py bdev_ha_create Ha2 ../ha/ha2_header.json -c 1
 
-sudo ./scripts/rpc.py bdev_ha_add_remote_node Ha2 --remote_node_id 2 --remote_node_type SYNC_REPLICA --remote_node_nqn nqn.2016-06.sw.ha2:node --remote_node_channel "tcp 192.168.0.103 4421"
+sudo ./scripts/rpc.py bdev_ha_add_remote_node Ha2 --remote_node_id 2 --remote_node_type SYNC_REPLICA --remote_node_channel "tcp 192.168.0.103 4421"
 
 sudo ./scripts/rpc.py bdev_ha_set_raft_leader_election Ha2
