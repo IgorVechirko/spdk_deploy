f5e2c04c37510a3ad00952098e5cf4fe2c2e2850
igor.vechirko <igor.vechirko@starwind.com>
2022-10-25 10:44:48 +0000

    update

--- confluence_samples/ha_deployment/node_2.sh
+++ confluence_samples/ha_deployment/node_2.sh
@@ -9,7 +9,7 @@ sudo ./scripts/rpc.py bdev_malloc_create -b ram_bdev 1024 512
 
 sudo ./scripts/rpc.py nvmf_create_subsystem nqn.2016-06.sw.ha:node -a -s SPDK00000000000001 -d SPDK_Controller1 --ana-reporting
 
-sudo ./scripts/rpc.py bdev_ha_create_header /root/wc/ha/ha_header.json --device_id 1 --device_nqn nqn.2016-06.sw.ha:ha1 --uuid 6d28acdf-7d3e-49b2-ac07-f5678160d7c3 --device_size 1GiB --local_node_id 2 --local_node_type SYNC_REPLICA --ha_creation_action create_new --local_node_data_replica ram_bdev
+sudo ./scripts/rpc.py bdev_ha_create_header /root/wc/ha/ha_header.json --device_id 1 --device_nqn nqn.2016-06.sw.ha:node --uuid 6d28acdf-7d3e-49b2-ac07-f5678160d7c3 --device_size 1GiB --local_node_id 2 --local_node_type SYNC_REPLICA --ha_creation_action create_new --local_node_data_replica ram_bdev
 sudo ./scripts/rpc.py bdev_ha_create Ha0 /root/wc/ha/ha_header.json
 
 #sudo ./scripts/rpc.py nvmf_subsystem_add_listener nqn.2016-06.sw.ha:node -t rdma -a 192.168.0.11 -s 4421
