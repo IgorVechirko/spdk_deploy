ca130938b40ec0512e2907033e5d8473594994c8
IgorVechirko <vechirkoigor@gmail.com>
2022-12-16 11:45:48 +0000

    add script for heart beat leader election set

---
+++ scripts/hb_set.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+. ./base.sh
+
+
+echo "Setting raft to node $1..."
+
+self_path=$(pwd)
+
+cd $(get_node $1 "spdk_path")
+
+sudo ./scripts/rpc.py bdev_ha_set_heart_beat_leader_election $(get_var "ha.name")
+
+
+
+cd $self_path
