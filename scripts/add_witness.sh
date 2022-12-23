5ab9879e8a6480dc97d1ffe01ecec5b146663180
igor.vechirko <igor.vechirko@starwind.com>
2022-12-23 17:26:21 +0000

    add args check

--- scripts/add_witness.sh
+++ scripts/add_witness.sh
@@ -2,14 +2,38 @@
 
 . ./base.sh
 
+help()
+{
+	echo "\nPlease call:\n\t $0 <node_id>"
+}
 
-echo "Adding smb witness..."
+if [ "$1" = "-h" ] || [ "$1" = "--help" ] || [ -z "$1" ]
+then
+	help
+	exit 1
+fi
+
+if [ $(check_is_node_exist $1) -eq "0" ]
+then
+	echo "\nNode $1 doesn't exist"
+	exit 1
+fi
+
+node_id=$1
+
+echo "\nAdding smb witness..."
 
 self_path=$(pwd)
 
-cd $(get_node $1 "spdk_path")
+cd $(get_node $node_id "spdk_path")
 
-rpc_args="bdev_ha_add_raft_smb_witness $(get_var 'ha.name') --witness-domain \"$(get_var 'smb_witness.domain')\" --witness-username \"$(get_var 'smb_witness.username')\" --witness-password \"$(get_var 'smb_witness.passwd')\" --witness-server \"$(get_var 'smb_witness.server')\" --witness-share \"$(get_var 'smb_witness.share')\" --witness-path \"$(get_var 'smb_witness.path')\""
+rpc_args="bdev_ha_add_raft_smb_witness $(get_var 'ha.name')"
+rpc_args="$rpc_args --witness-domain \"$(get_var 'smb_witness.domain')\""
+rpc_args="$rpc_args --witness-username \"$(get_var 'smb_witness.username')\""
+rpc_args="$rpc_args --witness-password \"$(get_var 'smb_witness.passwd')\""
+rpc_args="$rpc_args --witness-server \"$(get_var 'smb_witness.server')\""
+rpc_args="$rpc_args --witness-share \"$(get_var 'smb_witness.share')\""
+rpc_args="$rpc_args --witness-path \"$(get_var 'smb_witness.path')\""
 
 sudo echo ${rpc_args}|xargs python3 -u ./scripts/rpc.py
 
--- scripts/base.sh
+++ scripts/base.sh
@@ -42,3 +42,37 @@ get_node() {
 		get_var "defaults.$field"
 	fi
 }
+
+check_is_node_exist() {
+
+	ret=0
+	searching_node_id=$1
+
+	nodes=$(get_var "nodes")
+	nodes_count=$(echo $nodes | jq 'length')
+
+	for node_idx in `seq 0 $(($nodes_count-1))`
+	do
+		node_info=$(echo $nodes|jq ".[$node_idx]" -r)
+		node_id=$(echo $node_info|jq '.id')
+
+		if [ $node_id -eq $searching_node_id ]
+		then
+			ret=1
+			break
+		fi
+
+	done
+
+	echo "$ret"
+}
+
+#ret=$(check_is_node_exist $1)
+#echo $ret
+#
+#if [ $(check_is_node_exist $1) -eq "1" ]
+#then
+#	echo "yes"
+#fi
+
+#exit 1
