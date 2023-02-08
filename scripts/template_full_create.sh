e20c7009f580030f4abe85d79c769c804d0f2e50
igor.vechirko <igor.vechirko@starwind.com>
2023-02-08 13:33:58 +0000

    update

--- scripts/template_full_create.sh
+++ scripts/template_full_create.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-sh kill_tgt.sh 1
-#sh copy_build.sh 1
-sh run_tgt.sh 1
-sleep 2
-sh setup_spdk.sh 1
-sh create_ha.sh 1
-sh connect_remote_node.sh 2
-sh raft_set.sh 1
+sh kill_bin.sh ram512 1
+sh run_bin.sh ram512 1
+#sleep 3
+#sh set_debug_log.sh ram512 1
+sh setup_bin.sh ram512 1
+sh setup_ha.sh ram512 1
+#sh load_ha.sh ram512 1
+sh create_ha.sh ram512 1
+sh connect_remote_node.sh ram512 1 2
+#sh connect_remote_node.sh ram512 1 3
+#sh raft_set.sh ram512 1
+sh heartbeat_set.sh ram512 1
+
+#tail -f ../../ha/tgt_output.log
