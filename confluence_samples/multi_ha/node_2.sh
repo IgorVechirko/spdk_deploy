add0814d32f016c1ebb8043c5d421e56e699e6b2
igor.vechirko <igor.vechirko@starwind.com>
2023-02-22 13:52:20 +0000

    update confluence scripts

--- confluence_samples/multi_ha/node_2.sh
+++ confluence_samples/multi_ha/node_2.sh
@@ -6,31 +6,30 @@ sudo ./scripts/rpc.py nvmf_create_transport -t TCP
 sudo ./scripts/rpc.py nvmf_create_transport -t RDMA
 
 
-# create Ha1 in nqn.2016-06.sw.ha1:node subsystem
-sudo ./scripts/rpc.py bdev_malloc_create -b ram_bdev 512 512
-#sudo ./scripts/rpc.py bdev_nvme_attach_controller -b Nvme0 -t PCIe -a 0000:00:0e.0
+#Ha1
+sudo ./scripts/rpc.py bdev_malloc_create -b ha1_ram_bdev 1024 512
+#bdev_nvme_attach_controller -b Nvme0 -t PCIe -a 00.00.00.e8
 
-sudo ./scripts/rpc.py nvmf_create_subsystem nqn.2016-06.sw.ha:node -a -s SPDK00000000000001 -d SPDK_Controller1 --ana-reporting
+sudo ./scripts/rpc.py nvmf_create_subsystem nqn.2016-06.sw.ha:ha1 -a -s SPDK00000000000001 -d SPDK_Controller1 --ana-reporting
 
-sudo ./scripts/rpc.py bdev_ha_create_header /root/wc/ha/ha_header.json --device_id 1 --device_nqn nqn.2016-06.sw.ha:node --device_size 512MB --uuid 6d28acdf-7d3e-49b2-ac07-f5678160d7c3 --ha_creation_action create_new --local_node_id 2 --local_node_type SYNC_REPLICA --local_node_nsid 1 --local_node_data_replica ram_bdev --node_partners_channel "SYNC TCP 10.10.10.3 7654" --node_partners_channel "HEARTBEAT TCP 10.10.10.3 7654"
-sudo ./scripts/rpc.py bdev_ha_create Ha0 /root/wc/ha/ha_header.json
+sudo ./scripts/rpc.py bdev_ha_create_header /root/wc/ha/ha1_header.json --device_id 1 --device_nqn nqn.2016-06.sw.ha:ha1 --device_size 1GiB --uuid 6d28acdf-7d3e-49b2-ac07-f5678160d7c3 --ha_creation_action create_new --local_node_id 2 --local_node_nsid 1 --local_node_data_replica ha1_ram_bdev --node_partners_channel "SYNC TCP 30.30.30.102 9735"
+sudo ./scripts/rpc.py bdev_ha_create Ha0 /root/wc/ha/ha1_header.json
 
-sudo ./scripts/rpc.py bdev_ha_add_remote_node Ha0 --remote_node_id 1 --remote_node_type SYNC_REPLICA --partners_channel "SYNC TCP 10.10.10.2 7544" --partners_channel "HEARTBEAT TCP 10.10.10.2 7403"
+sudo ./scripts/rpc.py bdev_ha_append_node Ha1 --node_id 1 --node-nsid 1 --partners_channel "SYNC TCP 30.30.30.101 8523"
 
-sudo ./scripts/rpc.py bdev_ha_set_nodemajority Ha0
+sudo ./scripts/rpc.py bdev_ha_set_nodemajority Ha1
 
 
 
-# create Ha2 in nqn.2016-06.sw.ha2:node
-sudo ./scripts/rpc.py bdev_malloc_create -b ha2_1_node 512 512
-#sudo ./scripts/rpc.py bdev_nvme_attach_controller -b Nvme0 -t PCIe -a 0000:00:0e.0
+#Ha2
+sudo ./scripts/rpc.py bdev_malloc_create -b ha2_ram_bdev 1024 512
+#bdev_nvme_attach_controller -b Nvme0 -t PCIe -a 00.00.00.e8
 
-sudo ./scripts/rpc.py nvmf_create_subsystem nqn.2016-06.sw.ha2:node -a -s SPDK00000000000001 -d SPDK_Controller1 --ana-reporting
+sudo ./scripts/rpc.py nvmf_create_subsystem nqn.2016-06.sw.ha:ha2 -a -s SPDK00000000000001 -d SPDK_Controller1 --ana-reporting
 
-sudo ./scripts/rpc.py bdev_ha_create_header /root/wc/ha/ha2_header.json --device_id 2 --device_nqn nqn.2016-06.sw.ha2:node --device_size 512MB --uuid 6d28acdf-0000-49b2-ac07-f5678160d7c4 --ha_creation_action create_new --local_node_id 2 --local_node_type SYNC_REPLICA --local_node_nsid 1 --local_node_data_replica ha2_1_node --node_partners_channel "SYNC TCP 10.10.10.3 7654" --node_partners_channel "HEARTBEAT TCP 10.10.10.3 7654"
+sudo ./scripts/rpc.py bdev_ha_create_header /root/wc/ha/ha2_header.json --device_id 1 --device_nqn nqn.2016-06.sw.ha:ha2 --device_size 1GiB --uuid 6d28acdf-7d3e-49b2-ac07-f5678160d7c3 --ha_creation_action create_new --local_node_id 2 --local_node_nsid 1 --local_node_data_replica ha2_ram_bdev --node_partners_channel "SYNC TCP 30.30.30.102 9735" --node_partners_channel "HEARTBEAT TCP 20.20.20.102 1374"
+sudo ./scripts/rpc.py bdev_ha_create Ha0 /root/wc/ha/ha2_header.json
 
-sudo ./scripts/rpc.py bdev_ha_create Ha2 /root/wc/ha/ha2_header.json
-
-sudo ./scripts/rpc.py bdev_ha_add_remote_node Ha2 --remote_node_id 1 --remote_node_type SYNC_REPLICA --partners_channel "SYNC TCP 10.10.10.2 7544" --partners_channel "HEARTBEAT TCP 10.10.10.2 7403"
+sudo ./scripts/rpc.py bdev_ha_append_node Ha2 --node_id 1 --node-nsid 1 --partners_channel "SYNC TCP 30.30.30.101 8523" --partners_channel "HEARTBEAT TCP 20.20.20.101 6547"
 
 sudo ./scripts/rpc.py bdev_ha_set_heartbeat Ha2
