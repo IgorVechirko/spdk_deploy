fe1c7825b2b2b69320345565bd692eebe7c15394
igor.vechirko <igor.vechirko@starwind.com>
2023-02-22 15:36:23 +0000

    Update scripts

--- confluence_samples/smb_then_node_to_ha/node_1_2.sh
+++ confluence_samples/smb_then_node_to_ha/node_1_2.sh
@@ -8,4 +8,4 @@ cd /root/wc/sw_spdk
 sudo ./scripts/rpc.py bdev_ha_add_smb_witness Ha0 --smb_domain "" --smb_username "root" --smb_password "1234" --smb_server "192.168.0.131" --smb_share "smb_witness" --smb_path ""
 
 #this one for append new node to existing HA device 
-sudo ./scripts/rpc.py bdev_ha_append_node Ha0 --node_id 2 --node-nsid 1 --partners_channel "SYNC TCP 30.30.30.102 9735"
+sudo ./scripts/rpc.py bdev_ha_append_node Ha0 --node_id 2 --node_nsid 1 --partners_channel "SYNC TCP 30.30.30.102 9735"
