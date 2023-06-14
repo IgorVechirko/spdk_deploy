e2746d45c31a1effa7c030500ffbbc723d3e10fd
igor.vechirko <igor.vechirko@starwind.com>
2023-06-14 15:11:58 +0000

    update full create teamplate, remove unnecessary scripts

--- scripts/template_full_create.sh
+++ scripts/template_full_create.sh
@@ -2,6 +2,7 @@
 
 sh kill_bin.sh ram512 1
 sh run_bin.sh ram512 1
+sh remove_header.sh ram512 1
 #sleep 3
 #sh set_debug_log.sh ram512 1
 sh setup_bin.sh ram512 1
@@ -10,7 +11,7 @@ sh setup_ha.sh ram512 1
 sh create_ha.sh ram512 1
 sh connect_remote_node.sh ram512 1 2
 #sh connect_remote_node.sh ram512 1 3
-#sh raft_set.sh ram512 1
+#sh nodemajority_set.sh ram512 1
 sh heartbeat_set.sh ram512 1
 
 #tail -f ../../ha/tgt_output.log
