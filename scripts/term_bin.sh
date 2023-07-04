7b084458a82993e48fa28b2fa3f938544cef26cf
igor.vechirko <igor.vechirko@starwind.com>
2023-07-04 11:17:01 +0000

    add new term_bin.sh script for terminating service

---
+++ scripts/term_bin.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+. ./base.sh
+
+help()
+{
+	echo "\nPlease call:\n\t $0 <dev_name> <node_id>"
+}
+
+if [ "$1" = "-h" ] || [ "$1" = "--help" ] || [ -z "$1" ] || [ -z "$2" ]
+then
+	help
+	exit 1
+fi
+
+if [ -z "$get_dev_info $1" ]
+then
+	echo "\nDevice $1 doesn't exist"
+	exit 1
+fi
+
+if [ $(check_is_dev_node_exist $1 $2) -eq "0" ]
+then
+	echo "\nFor device $1 node $1 doesn't exist"
+	exit 1
+fi
+
+dev=$1
+node=$2
+
+echo "Terminating $dev device binary on $node node..."
+
+spdk_path=$(get_dev_node_field $dev $node "spdk_path")
+
+terminate_cmd="$spdk_path/scripts/rpc.py spdk_kill_instance SIGTERM"
+
+host_addr=$(get_dev_node_field $dev $node "ssh_ftp_addr")
+user=$(get_dev_node_field $dev $node "ssh_ftp_user")
+pass=$(get_dev_node_field $dev $node "ssh_ftp_pass")
+
+exe_on_host $host_addr $user $pass "$terminate_cmd"
